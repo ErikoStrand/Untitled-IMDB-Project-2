809e09d220495eb52a60978048f25a4e95854d92
Erik Strandberg <erikstrandberg153@gmail.com>
2024-01-25 21:03:15 +0100

    added new files.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#f6c615",
+        "accent-dark": "#d0a508",
+      },
+    },
+  },
+  plugins: [],
+};
