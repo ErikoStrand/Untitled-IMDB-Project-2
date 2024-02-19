4d56a95f93d2421f4919dc368c738bd8aa91607a
Erik Strandberg <erikstrandberg153@gmail.com>
2024-02-19 19:37:33 +0100

    added, days watched, and procentage of a year.
    its in big box, unsure how to showcase it right now its kinda messy>
    
    too much empty space.

--- src/statistics.js
+++ src/statistics.js
@@ -27,14 +27,14 @@ displayData();
 
 function displayData() {
   //movies
-  var movie = document.getElementById("movies");
-  var director = document.getElementById("directors");
-  var topDirector = document.getElementById("topDirectors");
+  let movie = document.getElementById("movies");
+  let director = document.getElementById("directors");
+  let topDirector = document.getElementById("topDirectors");
 
-  var movieWatchtimeMinutes = document.getElementById("movieWatchtimeMinutes");
-  var movieWatchtimeHours = document.getElementById("movieWatchtimeHours");
-  var averageRating = document.getElementById("movieAverageRating");
-  var averageRatingIMDB = document.getElementById("movieAverageRatingIMDB");
+  let movieWatchtimeMinutes = document.getElementById("movieWatchtimeMinutes");
+  let movieWatchtimeHours = document.getElementById("movieWatchtimeHours");
+  let averageRating = document.getElementById("movieAverageRating");
+  let averageRatingIMDB = document.getElementById("movieAverageRatingIMDB");
 
   for (let i = 0; i < 5 && i < Object.keys(movies["directors"]).length; i++) {
     const director = Object.keys(movies["directors"])[i];
@@ -52,11 +52,11 @@ function displayData() {
   averageRating.textContent = movies["averageRating"].toFixed(1);
   averageRatingIMDB.textContent = movies["averageRatingIMDB"].toFixed(1);
   //tv episodes
-  var show = document.getElementById("shows");
-  var showAverageRating = document.getElementById("showAverageRating");
-  var showAverageRatingIMDB = document.getElementById("showAverageRatingIMDB");
-  var showWatchtimeMinutes = document.getElementById("showWatchtimeMinutes");
-  var showWatchtimeHours = document.getElementById("showWatchtimeHours");
+  let show = document.getElementById("shows");
+  let showAverageRating = document.getElementById("showAverageRating");
+  let showAverageRatingIMDB = document.getElementById("showAverageRatingIMDB");
+  let showWatchtimeMinutes = document.getElementById("showWatchtimeMinutes");
+  let showWatchtimeHours = document.getElementById("showWatchtimeHours");
 
   showWatchtimeHours.textContent = shows["totalWatchtimeHours"];
   showWatchtimeMinutes.textContent = nFormatter(
@@ -68,17 +68,26 @@ function displayData() {
   showAverageRatingIMDB.textContent = shows["averageRatingIMDB"].toFixed(1);
 
   //general
-  var highestStreak = document.getElementById("highestStreak");
-  var currentStreak = document.getElementById("currentStreak");
-  var totalMedia = document.getElementById("totalMedia");
-  var totalAverageRating = document.getElementById("totalAverageRating");
-  var longestTitle = document.getElementById("longestTitle");
-  var shortestTitle = document.getElementById("shortestTitle");
-  var mostVotes = document.getElementById("mostVotes");
-  var mostVotesTitle = document.getElementById("mostVotesTitle");
-  var leastVotes = document.getElementById("leastVotes");
-  var leastVotesTitle = document.getElementById("leastVotesTitle");
+  let totalWatchtimeNumber =
+    movies["totalWatchtimeHours"] + shows["totalWatchtimeHours"];
+  let highestStreak = document.getElementById("highestStreak");
+  let currentStreak = document.getElementById("currentStreak");
+  let totalMedia = document.getElementById("totalMedia");
+  let totalAverageRating = document.getElementById("totalAverageRating");
+  let longestTitle = document.getElementById("longestTitle");
+  let shortestTitle = document.getElementById("shortestTitle");
+  let mostVotes = document.getElementById("mostVotes");
+  let mostVotesTitle = document.getElementById("mostVotesTitle");
+  let leastVotes = document.getElementById("leastVotes");
+  let leastVotesTitle = document.getElementById("leastVotesTitle");
+  let totalWatchtime = document.getElementById("totalWatchtime");
+  let totalWatchtimeDays = document.getElementById("totalWatchtimeDays");
+  let totalWatchtimeProcent = document.getElementById("totalWatchtimeProcent");
 
+  totalWatchtimeDays.textContent = (totalWatchtimeNumber / 24).toFixed(0);
+  totalWatchtime.textContent = totalWatchtimeNumber;
+  totalWatchtimeProcent.textContent =
+    ((totalWatchtimeNumber / 8765.81277) * 100).toFixed(1) + "%";
   highestStreak.textContent = generalData["streak"]["highestStreak"];
   currentStreak.textContent = generalData["streak"]["currentStreak"];
   totalMedia.textContent = generalData["totalMedia"];
