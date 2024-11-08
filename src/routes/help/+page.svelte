641acc2838901814f8c2c5769b8ce0093e267503
Erik Strandberg <erikstrandberg153@gmail.com>
2024-11-08 21:12:01 +0100

    added the thing i need in ? for tmdb

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,14 +1,17 @@
 <div
-	class="font-heebo mx-auto flex max-w-[500px] flex-col justify-center gap-6 font-light tracking-wide text-stone-50"
+	class="mx-auto flex max-w-[500px] flex-col justify-center gap-6 font-heebo font-light tracking-wide text-stone-50"
 >
-	<h1 class="font-archivo text-center text-4xl font-medium text-stone-50">Help & FAQ</h1>
-	<p>Critic Recap was made by Erik Strandberg. The application has no affiliation with IMDb.</p>
+	<h1 class="text-center font-archivo text-4xl font-medium text-stone-50">Help & FAQ</h1>
+	<p>
+		Critic Recap was made by Erik Strandberg. The application has no affiliation with IMDb, or any
+		other APIs it uses
+	</p>
 	<p>
 		If you are wondering why I made this, it's because i've always wanted to know how many hours
-		i've spend wathcing movies and shows and making a website is a great was to showcase the data.
+		i've spend watching movies and shows and making a website is a great was to showcase the data.
 	</p>
-	<div>
-		<h2 class="text-lg font-extrabold">Information Origin</h2>
+	<h2 class="text-lg font-extrabold">Information Origin</h2>
+	<div class="flex flex-col gap-4">
 		<p>
 			Information courtesy of IMDb
 			<a
@@ -18,5 +21,21 @@
 				href="https://www.imdb.com/">https://www.imdb.com</a
 			>. Used with permission.
 		</p>
+		<section>
+			<p>This site uses TMDB</p>
+			<p>
+				This website (program) uses TMDB APIs but is not endorsed, certified, or otherwise approved
+				by TMDB.
+			</p>
+			<p>
+				if you want to learn more about their organization visit
+				<a
+					target="_blank"
+					rel="noopener"
+					class="font-bold underline decoration-blue-500 decoration-2"
+					href="https://www.themoviedb.org/">https://www.themoviedb.org/</a
+				>.
+			</p>
+		</section>
 	</div>
 </div>
